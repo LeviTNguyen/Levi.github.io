ccb73ea302da4f60f2f0d4ee4587e889a23715c4
LeviTNguyen <romnguyen465@gmail.com>
2023-10-20 04:25:55 +0700

    first commit

---
+++ QLSV/CustomLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV
+{
+    internal class CustomLogin : Login
+    {
+        public CustomLogin(string username, string password) : base(username, password) { }
+        public override bool Authenticate()
+        {
+            return false;
+        }
+    }
+}
---
+++ QLSV/Teacher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV
+{
+    internal class Teacher
+    {
+        private string id;
+        private string name;
+        private int age;
+
+        public Teacher(string id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public string TeacherID { get; internal set; }
+        public string Name { get; internal set; }
+        public int Age { get; internal set; }
+
+        internal void PrintInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
